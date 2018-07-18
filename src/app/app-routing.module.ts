61c94f0c4b246dc6db162c1a73fb8408a791eb81
ChristaFox <christa.r.fox@me.com>
2018-07-17 18:12:29 -0700

    day 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroComponent } from './hero/hero.component';
+
+// const routes: Routes = [
+//   path
+// ]
 
 @NgModule({
   imports: [
